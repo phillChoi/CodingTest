8a7f1accf70a054039a2afef64e1649f028f12a7
Phillip <60904690+phillChoi@users.noreply.github.com>
2021-10-16 23:25:36 +1100

    Before testing

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const port = 3000;
 
+//-----Client Setup---------
 app.use(express.static('pages'));
+app.get('/index.html', function (req, res) {  
+    res.sendFile( __dirname + "/" + "index.html" );  
+});
 
-app.listen(port, () => {
-  console.log(`Server is on`);
+app.post('/orders' ,function (req, res){
+    //res.send(console.log("hello"));
+    response = {
+        model:req.query.type,
+        name:req.query.customerName
+    };
+    console.log(response);  
+    res.end(JSON.stringify(response));  
+});
+
+let server = app.listen( 8000, function() {
+    console.log('server started');
 });
