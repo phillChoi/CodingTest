930750b313a5b9c44d3fc1fc37c7d5ac452a5739
Phillip <60904690+phillChoi@users.noreply.github.com>
2021-10-17 00:06:00 +1100

    First GET request
    Testing out GET method

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const app = express();
 
 //-----Client Setup---------
 app.use(express.static('pages'));
-app.get('/index.html', function (req, res) {  
-    res.sendFile( __dirname + "/" + "index.html" );  
-});
 
-app.post('/orders' ,function (req, res){
-    //res.send(console.log("hello"));
-    response = {
-        model:req.query.type,
-        name:req.query.customerName
-    };
-    console.log(response);  
-    res.end(JSON.stringify(response));  
-});
-
+//----Server Setup------
 let server = app.listen( 8000, function() {
     console.log('server started');
 });
+
+
+app.get('/orders', function (req, res) {  
+    response = {  
+        id: 1,
+        title:req.query.title,
+        date:req.query.date,
+        type:req.query.type,  
+        name:req.query.customer
+    };  
+    console.log(response);  
+    res.end(JSON.stringify(response));  
+})  
