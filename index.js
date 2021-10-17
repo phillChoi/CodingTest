f1cf156729c339fbca59b664b020f58715baa051
Phillip <60904690+phillChoi@users.noreply.github.com>
2021-10-17 23:11:48 +1100

    Added Post method
    Post form added according to specs, results are added to JSON file to simulate a database.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 //-----Client Setup---------
 app.use(express.static('pages'));
+
 
 //----Server Setup------
 let server = app.listen( 8000, function() {
@@ -11,14 +13,30 @@ let server = app.listen( 8000, function() {
 });
 
 
-app.get('/orders', function (req, res) {  
+app.post('/orders', function (req, res) {  
     response = {  
         id: 1,
-        title:req.query.title,
-        date:req.query.date,
-        type:req.query.type,  
-        name:req.query.customer
+        title:req.body.title,
+        date:req.body.date,
+        type:req.body.type,  
+        name:req.body.customer
     };  
+    let data = JSON.stringify(response);
     console.log(response);  
+    fs.writeFileSync('./output/orders.json', data);
     res.end(JSON.stringify(response));  
-})  
+}); 
+
+
+app.get('/orders', function (req, res){
+    let id = req.query.id;
+    response = {  
+        id: 1,
+        title:4,
+        date:3,
+        type:2, 
+        name:1
+    };  
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
