c08f7f26a3840610166bbb868e4e42583b9b897b
Phillip <60904690+phillChoi@users.noreply.github.com>
2021-10-18 19:12:33 +1100

    Post Method Complete
    Post request completed.
    Successfully adding request to JSON file output to simulate database.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const directory = "./output/orders.json";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
+
 //-----Client Setup---------
 app.use(express.static('pages'));
-
 
 //----Server Setup------
 let server = app.listen( 8000, function() {
     console.log('server started');
 });
-
 
 app.post('/orders', function (req, res) {  
     response = {  
@@ -20,23 +20,58 @@ app.post('/orders', function (req, res) {
         date:req.body.date,
         type:req.body.type,  
         name:req.body.customer
-    };  
-    let data = JSON.stringify(response);
-    console.log(response);  
-    fs.writeFileSync('./output/orders.json', data);
-    res.end(JSON.stringify(response));  
+    };
+    try{
+        //Check if there already is an output JSON file (used to simulate database).
+        if (fs.existsSync(directory)){
+            console.log("This file exists");
+            fs.readFile(directory, 'utf-8', (error, jsonFile) => {
+                if (error) {
+                    console.log("Error reading file: " + error);
+                } else {
+                    let data = JSON.parse(jsonFile);//Store existing JSON to variable
+                    response.id = data.length + 1; //Update ID for order
+                    data.push(response);
+                    let order = JSON.stringify(data, null, 2);
+                    //Update existing data
+                    fs.writeFile(directory, order, err2 => {
+                        if (err2){
+                            console.log("There has been an error: "+err2);
+                        }else {
+                            console.log("File Successfully updated.")
+                            res.end(JSON.stringify(response)); 
+                        }
+                    })
+                }
+            })
+    
+        } else { 
+            //Create JSON file if not exists   
+            let databaseArray = [];
+            databaseArray.push(response);
+            //console.log(databaseArray);  
+            let order = JSON.stringify(databaseArray, null, 2);
+            fs.writeFile(directory, order, err5 => {
+                if(err5){
+                    console.log("There has been an error: "+err5);
+                } else {    
+                    console.log("Database Successfully created at: "+directory);
+                }
+            });
+            res.end(JSON.stringify(response)); 
+        };
+    } catch(err){
+        console.log(err);
+    }; 
 }); 
 
 
-app.get('/orders', function (req, res){
+app.get('/orders/:id', function (req, res){
     let id = req.query.id;
-    response = {  
-        id: 1,
-        title:4,
-        date:3,
-        type:2, 
-        name:1
-    };  
+
     console.log(response);
     res.end(JSON.stringify(response));
+});
+
+app.get('orders/:type/:date',function(req,res){
 });
